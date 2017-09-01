35d146102c385afa9c0491d5cd6910d295a13bb3
ladofa9@gmail.com <ladofa9@gmail.com>
2017-09-01 16:58:32 +0900

    Add project files.

---
+++ cs-registry-wrapper/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_registry_wrapper
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
